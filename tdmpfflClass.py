4a7299f040afb43c8b85f706b1db7539b42c1556
pbullion <pbullion@gmail.com>
2017-01-06 14:15:10 -0600

    typo

--- tdmpfflClass.py
+++ tdmpfflClass.py
@@ -69,7 +69,7 @@ class Owners(object):
     def standings(year):
         db = pg.DB(host=DBHOST, user=DBUSER, passwd=DBPASS, dbname=DBNAME)
         query = db.query(
-            "select team,owner,sum(wins) wins,sum(loss) loss,idowners from winloss where 'year'='%s" group by owner,idowners order by sum(wins) desc" % 'year')
+            "select team,owner,sum(wins) wins,sum(loss) loss,idowners from winloss where 'year'='%s'" group by owner,idowners order by sum(wins) desc" % 'year')
         result_list = query.namedresult()
         db.close()
         return result_list
