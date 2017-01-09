aefe5dda4c82c733b309bc72237672f48c5daa3f
koneil612 <kellis1286@gmail.com>
2017-01-09 16:01:43 -0600

    organizing folders

--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
 import nflgame
 
-games = nflgame.games(2013, week=1)
+games = nflgame.games(2016, week=14)
 players = nflgame.combine_game_stats(games)
-for p in players.rushing().sort('rushing_yds').limit(5):
+for p in players.rushing().sort('rushing_yds').limit(15):
     msg = '%s %d carries for %d yards and %d TDs'
     print msg % (p, p.rushing_att, p.rushing_yds, p.rushing_tds)
 
