57d783e1e50b05e7a5b3bda5951ccc2416d16936
pbullion <pbullion@gmail.com>
2017-01-09 14:34:40 -0600

    got the standings and the year links to work.
    everything is pulling how i want now
    
    on to styling

--- tdmpfflClass.py
+++ tdmpfflClass.py
@@ -55,7 +55,7 @@ class Owners(object):
     @staticmethod
     def alltime():
         db = pg.DB(host=DBHOST, user=DBUSER, passwd=DBPASS, dbname=DBNAME)
-        query = db.query("select owner,sum(wins) wins,sum(loss) loss,idowners from winloss group by owner,idowners order by sum(wins) desc")
+        query = db.query("select owner,sum(wins) wins,sum(loss) loss,idowners,ROUND(sum(wins)*1.0/sum(wins+loss)::numeric,4) percent from winloss group by owner,idowners order by percent desc")
         result_list = query.namedresult()
         db.close()
         return result_list
